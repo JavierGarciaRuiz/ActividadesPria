77799d3ec8f805ed32ec7a09e436d7d9a3d7bf76
Javier García Ruiz <99357715+JavierGarciaRuiz@users.noreply.github.com>
2024-11-08 19:38:00 +0100

    Add files via upload

---
+++ C#/ActividadesCondicionales/Actividad20/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+int numero = 5;
+
+string contenerPalabra;
+
+if (numero > 0 && numero < 10000)
+{
+    contenerPalabra = numero.ToString("G", new CultureInfo ("es-ES"));
+    if (contenerPalabra.Contains("cinco"))
+    {
+        Console.WriteLine("Si.");
+    }
+    else
+    {
+        Console.WriteLine("No.");
+    }
+}
+else
+{
+    Console.WriteLine("El numero no entra en el rango permitido");
+}
