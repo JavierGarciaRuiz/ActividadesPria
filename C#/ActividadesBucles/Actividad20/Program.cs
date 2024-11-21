78274b26a23ca4486e4925553e2a52d2b0bb9124
Javier García Ruiz <99357715+JavierGarciaRuiz@users.noreply.github.com>
2024-11-21 12:18:12 +0100

    Add files via upload

---
+++ C#/ActividadesBucles/Actividad20/Program.cs
@@ -0,0 +1,20 @@
+﻿List<int> lista = new List<int> { 2, 7, 6, 2, 6, 7, 2, 7 };
+int suma = 0;
+bool boleano = false;
+
+foreach (int i in lista)
+{
+    if (i == 6)
+    {
+        boleano = true;
+    }
+    if (boleano != true)
+    {
+        suma = i + suma;
+    }
+    if(i == 7 && boleano)
+    {
+        boleano = false;
+    }
+}
+Console.WriteLine(suma);
